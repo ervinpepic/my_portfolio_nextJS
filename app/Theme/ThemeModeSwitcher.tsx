7116b7897401de2ec1368f8ae657bd5e07a83b68
Ervin Pepic <pepic_ervin@hotmail.com>
2024-11-11 19:52:23 +0100

    Fixed hydratation warning on global

--- app/Theme/ThemeModeSwitcher.tsx
+++ app/Theme/ThemeModeSwitcher.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useTheme } from "next-themes";
+import { useTheme,  } from "next-themes";
 import { useEffect, useState } from "react";
 import { IconContext } from "react-icons";
 import { PiMoonThin, PiSunThin } from "react-icons/pi";
@@ -7,18 +7,20 @@ import { PiMoonThin, PiSunThin } from "react-icons/pi";
 const themeBtnStyle = "w-10 h-10 fixed right-5 top-5 px-2 rounded-full hover:scale-150 active:scale-300 duration-300 bg-gray-700 dark:bg-slate-200"
 const ThemeModeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
   }, []);
-  if (!mounted) return null;
+  if (!mounted)
+    return <div className="min-h-screen bg-slate-200 dark:bg-gray-700"></div>;
+  const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <button
       className={themeBtnStyle}
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
     >
-      {theme && theme === "dark" ? (
+      {currentTheme && currentTheme === "dark" ? (
         <IconContext.Provider value={{ className: "text-slate-700 text-2xl" }}>
           <PiSunThin />
         </IconContext.Provider>
---
+++ app/not-found.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function NotFound() {
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // To prevent hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted)
+    return <div className="min-h-screen bg-slate-200 dark:bg-gray-700"></div>;
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center justify-center transition-colors duration-300 ${
+        currentTheme === "dark"
+          ? "bg-gray-700 text-slate-200"
+          : "bg-slate-200 text-gray-700"
+      }`}
+    >
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="mb-6">
+          Oops! The page you are looking for doesn't exist. Please go back and
+          try some other page...
+        </p>
+        <Link href="/">
+          <button className="py-2 px-6 rounded bg-gray-700 dark:bg-slate-200 text-slate-200 dark:text-gray-700 hover:bg-gray-600 dark:hover:bg-slate-300 hover:duration-300">
+            Go Back Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
